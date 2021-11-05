927ea6717d41068567655689c4c5ce640501c506
peteranton1 <peteranton1@yahoo.com>
2021-11-05 20:14:59 +0000

    Initial example

---
+++ rest-reactive/src/main/java/com/example/restreactive/Greeting.java
@@ -0,0 +1,4 @@
+package com.example.restreactive;
+
+public record Greeting(String name) {
+}
---
+++ rest-reactive/src/main/java/com/example/restreactive/RestReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.restreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestReactiveApplication.class, args);
+	}
+
+}
