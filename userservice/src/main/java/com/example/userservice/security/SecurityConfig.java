546ce7cdd8362452628e53f585d881d3c2458948
peteranton1 <peteranton1@yahoo.com>
2022-03-05 02:03:53 +0000

    security part3

--- userservice/src/main/java/com/example/userservice/security/SecurityConfig.java
+++ userservice/src/main/java/com/example/userservice/security/SecurityConfig.java
@@ -7,13 +7,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
@@ -32,10 +32,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
+
+    CustomAuthenticationFilter authenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+    authenticationFilter.setFilterProcessesUrl("/api/login");
+
     http.csrf().disable();
     http.sessionManagement().sessionCreationPolicy(STATELESS);
-    http.authorizeRequests().anyRequest().permitAll();
-    http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+
+    http.authorizeRequests()
+        .antMatchers("/api/login","/api/users")
+        .permitAll();
+    http.authorizeRequests()
+        .antMatchers(GET, "/api/user/**")
+        .hasAnyAuthority("ROLE_USER");
+    http.authorizeRequests()
+        .antMatchers(POST, "/api/user/save/**")
+        .hasAnyAuthority("ROLE_ADMIN");
+    http.authorizeRequests().anyRequest().authenticated();
+
+    http.addFilter(authenticationFilter);
   }
 
   @Bean
