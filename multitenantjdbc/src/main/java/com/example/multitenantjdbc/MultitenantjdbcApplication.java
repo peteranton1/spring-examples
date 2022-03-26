eb0e19845b92fbab139b5ccee3c9d39ae7213cd0
peteranton1 <peteranton1@yahoo.com>
2022-03-26 15:40:41 +0000

    Initial

---
+++ multitenantjdbc/src/main/java/com/example/multitenantjdbc/MultitenantjdbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.multitenantjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultitenantjdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultitenantjdbcApplication.class, args);
+	}
+
+}
